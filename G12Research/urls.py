e0dce8bb162f30daf37f2a1534199fcdd2c851ab
SiopaoEnjoyer <lanzarthurgaurana@gmail.com>
2026-01-26 13:32:14 +0800

    robots.txt

--- G12Research/urls.py
+++ G12Research/urls.py
@@ -20,15 +20,48 @@ from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import serve_pdf
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap, Sitemap
+from research.models import ResearchPaper
+
+# Define sitemaps
+class StaticViewSitemap(Sitemap):
+    priority = 0.8
+    changefreq = 'weekly'
+    
+    def items(self):
+        return ['home', 'about', 'privacy-policy', 'terms', 'research', 'search']
+    
+    def location(self, item):
+        if item == 'home':
+            return '/'
+        elif item == 'research':
+            return '/research/'
+        elif item == 'search':
+            return '/search/'
+        else:
+            return f'/{item}/'
+
+# Sitemap for research papers
+research_dict = {
+    'queryset': ResearchPaper.objects.all().order_by('-publication_date'),
+    'date_field': 'publication_date',
+}
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'research': GenericSitemap(research_dict, priority=0.9, changefreq='monthly'),
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('research.urls')),
     path('accounts/', include(('accounts.urls','accounts'), namespace='accounts')),
     path('favicon.ico', RedirectView.as_view(url='/static/research/img/trinity.ico')),
+    path('robots.txt', RedirectView.as_view(url='/static/robots.txt', permanent=True)),
     path('media/<path:path>', serve_pdf, name='serve_pdf'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
